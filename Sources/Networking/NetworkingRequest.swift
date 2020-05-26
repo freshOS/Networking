9b97e6d1483e3c0aa6e05ff7c483610d85d47703
s4cha <sachadso@gmail.com>
2020-05-26 16:01:48 +0200

    Percent-encode params

--- Sources/Networking/NetworkingRequest.swift
+++ Sources/Networking/NetworkingRequest.swift
@@ -143,7 +143,7 @@ public class NetworkingRequest: NSObject {
         }
         
         if httpVerb != .get && multipartData == nil {
-            request.httpBody = postString().data(using: .utf8)
+            request.httpBody = percentEncodedString().data(using: .utf8)
         }
         
         // Multipart
@@ -155,18 +155,34 @@ public class NetworkingRequest: NSObject {
         return request
     }
     
-    func postString() -> String {
+
+    func percentEncodedString() -> String {
         return params.map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
             if let array = value as? [CustomStringConvertible] {
-                return array.map { "\(key)[]=\($0)" }.joined(separator: "&")
+                return array.map { entry in
+                    let escapedValue = "\(entry)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+                    return "\(key)[]=\(escapedValue)" }.joined(separator: "&"
+                )
             } else {
-                return "\(key)=\(value)"
+                let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+                return "\(escapedKey)=\(escapedValue)"
             }
         }
         .joined(separator: "&")
     }
 }
 
+// Thansks to https://stackoverflow.com/questions/26364914/http-request-in-swift-with-post-method
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
 
 extension NetworkingRequest: URLSessionTaskDelegate {
     public func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
