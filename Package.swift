f37e76a60e9e9751f9c2b1ca5f2924004e7cdda6
s4cha <sachadso@gmail.com>
2020-09-23 14:12:01 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Networking",
-    platforms: [.iOS(.v13), .macOS(SupportedPlatform.MacOSVersion.v10_15)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [.library(name: "Networking", targets: ["Networking"])],
     targets: [
         .target(name: "Networking"),
