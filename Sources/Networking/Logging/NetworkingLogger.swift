2c72d2cf63647be2c2ce46d25d3f64b847c71903
MaxenceLvl <maxence.levelu@outlook.fr>
2021-01-25 17:39:12 +0100

    Add the cURL command to the debug logs

--- Sources/Networking/Logging/NetworkingLogger.swift
+++ Sources/Networking/Logging/NetworkingLogger.swift
@@ -20,6 +20,10 @@ class NetworkingLogger {
             print("\(verb) '\(url.absoluteString)'")
             logHeaders(request)
             logBody(request)
+
+        }
+        if logLevels == .debug {
+            logCurl(request)
         }
     }
 
@@ -55,4 +59,34 @@ class NetworkingLogger {
             print("\(urlResponse.statusCode) '\(url.absoluteString)'")
         }
     }
+    
+    private func logCurl(_ urlRequest: URLRequest) {
+        print(urlRequest.toCurlCommand())
+    }
+}
+
+extension URLRequest {
+
+    /**
+        Heavily inspired from : https://gist.github.com/shaps80/ba6a1e2d477af0383e8f19b87f53661d
+     */
+    public func toCurlCommand() -> String {
+        guard let url = url else { return "" }
+        var command = ["curl \"\(url.absoluteString)\""]
+
+        if let method = httpMethod, method != "GET" && method != "HEAD" {
+            command.append("-X \(method)")
+        }
+
+        allHTTPHeaderFields?
+            .filter { $0.key != "Cookie" }
+            .forEach { command.append("-H '\($0.key): \($0.value)'")}
+
+        if let data = httpBody, let body = String(data: data, encoding: .utf8) {
+            command.append("-d '\(body)'")
+        }
+
+        return command.joined(separator: " \\\n\t")
+    }
+
 }
